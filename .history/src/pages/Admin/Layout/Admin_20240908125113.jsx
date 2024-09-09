164347088eb09b74ee00dccdc20ef34c11ba2a25
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2024-09-09 21:48:36 +0700

    Update CRUD user v3

---
+++ .history/src/pages/Admin/Layout/Admin_20240908125113.jsx
@@ -0,0 +1,44 @@
+import "../../../assets/css/styles.min.css";
+import "../../../assets/libs/jquery/dist/jquery.min.js";
+import "../../../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../../assets/js/sidebarmenu.js";
+import "../../../assets/js/app.min.js";
+import "../../../assets/libs/apexcharts/dist/apexcharts.js";
+import "../../../assets/libs/simplebar/dist/simplebar.js";
+import "../../../assets/js/dashboard.js";
+import LeftMenu from "../LeftMenu/LeftMenu.jsx";
+import Header from "../Header/Header.jsx";
+import Dashboard from "../Dashboard/Dashboard.jsx";
+import Users from "../Users/Users.jsx";
+import Roles from "../Roles/Roles.jsx";
+
+const Admin = () => {
+  const isUserRoute = location.pathname === "/admin/users";
+  const isDashboardRoute = location.pathname === "/admin/dashboard";
+  const isRoleRoute = location.pathname === "/admin/roles";
+  const isInsUpRoleRoute = location.pathname === "/admin/roles";
+  return (
+    <div
+      className="page-wrapper"
+      id="main-wrapper"
+      data-layout="vertical"
+      data-navbarbg="skin6"
+      data-sidebartype="full"
+      data-sidebar-position="fixed"
+      data-header-position="fixed"
+    >
+      {/* Sidebar Start  */}
+      <LeftMenu></LeftMenu>
+      {/* Sidebar End  */}
+      {/* Main wrapper  */}
+      <div className="body-wrapper">
+        <Header></Header>
+        {isDashboardRoute && <Dashboard />}
+        {isUserRoute && <Users />}
+        {isRoleRoute && <Roles />}
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ .history/src/pages/Admin/Roles/InsUpRole_20240908123157.jsx
@@ -0,0 +1,5 @@
+const InsUpRole = ()=>{
+    return()
+}
+
+export default InsUpRole;
